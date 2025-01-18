5e1731f9f554bb321a56316ea23932acebea7ebc
Henrique Francisco Venzon <143184156+Henrique-Venzon@users.noreply.github.com>
2025-01-18 17:30:45 -0300

    ajustes

--- js/loop-video.js
+++ js/loop-video.js
@@ -1,13 +1,10 @@
-window.addEventListener("DOMContentLoaded", () => {
+ window.addEventListener("DOMContentLoaded", () => {
     const video = document.getElementById("bgVideo");
 
-    // Garante que o vídeo seja reproduzido sem mostrar duração ou controles
+    video.removeAttribute("controls");
+    
     video.play().catch(() => {
-      // Em caso de falha, tenta reproduzir novamente com mutação garantida
       video.muted = true;
       video.play();
     });
-
-    // Remove qualquer atributo de controle que possa causar exibição visual
-    video.removeAttribute("controls");
   });
